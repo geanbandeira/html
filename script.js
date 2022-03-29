0b7376db9e59b5eea91c78df0a0abd7bdf1d069b
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2022-03-29 03:09:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = 16//data.getHours() 
+msg.innerHTML = `Agora são ${hora} horas.`
+if(hora >=0 && hora <12){
+    img.src='cafe.png'
+    document.body.style.background = '#e2cd9f'
+}else if (hora >=12 && hora <=18){
+    img.src='almoco.png'
+    document.body.style.background = '#b9846f'
+}else { 
+    img.src='jantar.png'
+    document.body.style.background = '#515154'
+    }  
+} 
